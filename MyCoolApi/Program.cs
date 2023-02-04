c3ec8cb4c2b623760043c5d82468e0aba7823dee
Tim Heuer <tim@timheuer.com>
2023-02-04 01:02:12 +0000

    Remove comments

--- MyCoolApi/Program.cs
+++ MyCoolApi/Program.cs
@@ -2,8 +2,6 @@ using MyCoolApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
