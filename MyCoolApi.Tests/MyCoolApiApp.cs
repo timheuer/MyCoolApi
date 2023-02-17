4c8d67bc3340e1adcfa3fcb39ff2083b6fdcd9ec
Tim Heuer <tim@timheuer.com>
2023-02-16 16:07:46 -0800

    Add line to CreateHost method to address remote test scenario failure.

--- MyCoolApi.Tests/MyCoolApiApp.cs
+++ MyCoolApi.Tests/MyCoolApiApp.cs
@@ -5,7 +5,10 @@ namespace MyCoolApi.Tests;
 
 class MyCoolApiApp : WebApplicationFactory<Program> {
     protected override IHost CreateHost(IHostBuilder builder) {
+        
+        // without this line below, certain remote test scenarios will fail
         builder.UseContentRoot(Directory.GetCurrentDirectory());
+
         return base.CreateHost(builder);
     }
 }
